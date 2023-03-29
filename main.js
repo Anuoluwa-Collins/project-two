71ab76c16a8733249c96a162a5fe286055b136df
Anuoluwa-Collins <112343782+Anuoluwa-Collins@users.noreply.github.com>
2023-03-29 22:58:50 +0100

    day 11

--- main.js
+++ main.js
@@ -8,3 +8,21 @@ document.addEventListener( 'DOMContentLoaded', function () {
           },
     } ).mount();
   } );
+
+
+  gsap.registerPlugin(ScrollTrigger);
+
+const sections = gsap.utils.toArray(".panel"),
+  container = document.querySelector(".container");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    end: () => "+=" + container.offsetWidth,
+  },
+});
